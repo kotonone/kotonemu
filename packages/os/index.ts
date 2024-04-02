6252b371abf2a84731bdcb7ac961f61be440af27
Kotone Fami <fami@ktnfm.com>
2024-04-02 21:57:22 +0900

    feat: ✨ Added kernel panic.

--- packages/os/index.ts
+++ packages/os/index.ts
@@ -395,16 +395,23 @@ export default function ShalfeltOS(terminal: Terminal): { options: EmulatorInit,
                                 protected: true,
 
                                 async onStart(lib) {
-                                    const info = this.uname();
-                                    lib.io.write(`\n${info.os_name} ${info.os_version}\n`);
-                                    lib.io.write("Copyright (C) 2024 Kotonone and ShalfeltOS contributors\n\n");
+                                    try {
+                                        const info = this.uname();
+                                        lib.io.write(`\n${info.os_name} ${info.os_version}\n`);
+                                        lib.io.write("Copyright (C) 2024 Kotonone and ShalfeltOS contributors\n\n");
 
-                                    await this.spawn(async function() {
-                                        await this.exec("/bin/login", [], {
-                                            PWD: "/",
-                                            PATH: "/bin:/sbin"
+                                        await this.spawn(async function() {
+                                            await this.exec("/bin/login", [], {
+                                                PWD: "/",
+                                                PATH: "/bin:/sbin"
+                                            });
                                         });
-                                    });
+                                    } catch (e) {
+                                        console.error(e);
+                                        terminal.write(`\x1b[2J\x1b[H\x1b[0m\x1b[40m  ${"\x1b[43m  \x1b[40m  ".repeat(10)}\n\n`);
+                                        terminal.write(`\x1b[0m  \x1b[1m\x1b[4m\x1b[33mShalfeltOS Kernel Panic\n\n`);
+                                        terminal.write(`\x1b[0m  ${(e as Error).stack}`);
+                                    }
                                 }
                             }
                         ]
